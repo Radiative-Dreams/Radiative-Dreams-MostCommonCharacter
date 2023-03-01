88e4bb29d2ddee0af7c98efdaac4ba217f32c332
Rey V <radiativedreams@gmail.com>
2023-03-01 02:12:35 +0000

    Completed Common Char

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,4 +1,5 @@
-
+import java.util.HashMap;
+import java.util.Map;
 public class MostCommonCharacter {
     /**
      * Find the most common character in str.
@@ -8,6 +9,25 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        HashMap<Character, Integer> vals = new HashMap<>();
+        for( int i = 0; i < str.length(); i++)
+        {
+            if( vals.get(str.charAt(i)) == null ) vals.put(str.charAt(i), 1);
+            else
+            {
+                vals.put(  str.charAt(i), vals.get( str.charAt(i) )+1  );
+            }
+        }
+        //System.out.println(vals + "\n");
+        int max = 0;
+        char x = ' ';
+        for (Map.Entry<Character, Integer> it : vals.entrySet() ) {
+            if(max < it.getValue()) 
+            {
+                max = it.getValue();
+                x = it.getKey();
+            }
+        }
+        return x;
     }
 }
